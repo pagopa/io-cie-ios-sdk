6acbbc729df0786ebcc2d8bc4e85b2f2424aaefe
Matteo Boschi <mbmaken@gmail.com>
2020-09-21 09:53:07 +0200

    library as framework

---
+++ ciesdk/iociesdkios/Test.swift
@@ -0,0 +1,20 @@
+//
+//  Test.swift
+//  iociesdkios
+//
+//  Created by Ugo Chirico on 17/09/2020.
+//  Copyright © 2020 ugos. All rights reserved.
+//
+
+import Foundation
+
+public class Test {
+
+public static let TAG_ERROR_SESSION_INVALIDATED : UInt16 = 10
+public static let TAG_ERROR_NFC_NOT_SUPPORTED : UInt16 = 11
+
+@objc public static func decodeError( error: UInt16) -> String {
+    
+    return "0"
+}
+}
